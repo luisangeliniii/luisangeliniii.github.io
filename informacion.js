b62e647db431190d1e1b5725125306416f1f5a29
ADMIN <luisangeliniii@gmail.com>
2023-02-18 19:21:07 -0300

    CuViMikeV0.1

---
+++ informacion.js
@@ -0,0 +1,10 @@
+function Idiomas(){
+document.getElementById("InfoExtra").innerHTML=("Ingles, Italiano, Frances, Español")
+}
+
+function Deportes(){
+    document.getElementById("InfoExtra").innerHTML=("bodyboarder, Surf, Futbol")
+    }
+    function DatosPersonales(){
+        document.getElementById("InfoExtra").innerHTML=("17 de mayo de 1963 tiene 59 años, nacio en Hawai Estados Unidos ")
+    }
